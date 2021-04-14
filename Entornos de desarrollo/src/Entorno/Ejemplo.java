a00c8867871fd58b1a96501c54574af158af71d7
Alumno <Alumno@A29-108>
2021-04-14 18:22:22 +0200

    Salida por pantalla

---
+++ Entornos de desarrollo/src/Entorno/Ejemplo.java
@@ -0,0 +1,16 @@
+package Entorno;
+
+/**
+ * @author Ruth191
+ */
+
+public class Ejemplo {
+
+	/**
+	 * Esto es una manera de corregir los cambios.
+	 */
+
+	private String Mesa;
+	private String Dolores;
+	
+}
